102bb8870d2cdb528c7be852a59ddd01e984ea82
SpiderEvgn <spiderz825@hotmail.com>
2015-11-06 00:30:40 +0800

    Create Client module

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
+  devise_for :clients
   devise_for :users
   get 'board/help'
-
+  
 # For test   / Aaron
   get 'profiles/index'
-  
+  get 'client_board/index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
