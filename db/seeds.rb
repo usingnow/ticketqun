0a9db2fabc7df50f2b2248b77197e98b55337bbc
Jimmy Xu <jianmin.xu@usingnow.com>
2015-11-10 16:45:13 +0800

    revised the seeds.rb for new Profile

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,53 +6,28 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-
-# class ReviseProfile < ActiveRecord::Migration
-#   def change
-#     drop_table :profiles
-
-#     create_table :profiles do |t|
-#       t.string :profile_id
-#       t.string :first_name
-#       t.string :last_name
-#       t.boolean :gender
-#       t.boolean :mariage
-#       t.date :birthday
-#       t.string :chinese_id_number
-#       t.string :chinese_passport_number
-
-#       t.string :occupation
-#       t.string :position
-#       t.string :employer
-
-#       t.string :country
-#       t.string :province
-#       t.string :city
-#       t.string :address
-#       t.string :zipcode
-
-#       t.timestamps null: false
-#     end
-#   end
-# end
-
 Profile.delete_all
-Profile.create!(
-	profile_id:                 '10000',
-  first_name:                 'Jimmy',
-  last_name:                  'Xu',
-  gender:                     true,
-  mariage:                    true,
-  birthday:                   '1980-03-25',
-  chinese_id_number:          '3010102198003252439',
-  chinese_passport_number:    'G12345678',
-  occupation:                 '信息科技',
-  position:                   '项目执行总监',
-  employer:                   '国际商业机器',
-  country:                    '中国',
-  province:                   '上海',
-  city:                       '市辖区',
-  address:                    '陆家嘴环路1388号16楼',
-  zipcode:                    '200001',)
 
+l_names = ['刘','曹','孙','关','张','赵','马','黄','诸葛','魏']
+f_names = ['备','操','权','羽','飞','云','超','忠','亮','延']
 
+(0..9).each do |n|
+  Profile.create!(profile_id:                 '10000',
+                  first_name:                 f_names[n],
+                  last_name:                  l_names[n],
+                  gender:                     true,
+                  mariage:                    true,
+                  birthday:                   '1980-03-25',
+                  chinese_id_number:          '301010219800325243' + n.to_s,
+                  chinese_passport_number:    'G12345678',
+                  occupation:                 '信息科技',
+                  position:                   '项目执行总监',
+                  employer:                   '国际商业机器',
+                  country:                    '中国',
+                  province:                   '上海',
+                  city:                       '市辖区',
+                  address:                    '陆家嘴环路1388号16楼' + n.to_s + '座',
+                  zipcode:                    '20000' + n.to_s,
+                  email:                      'mail.address.' + n.to_s + '@mail.com',
+                  user_id:                    (n+1).to_s,)
+end
