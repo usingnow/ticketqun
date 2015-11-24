5f653eefd879c275dbda47b3e96ccc3c06269715
SpiderEvgn <spiderz825@hotmail.com>
2015-11-24 22:06:17 +0800

    link employee to profile

--- db/seeds.rb
+++ db/seeds.rb
@@ -29,5 +29,6 @@ f_names = ['备','操','权','羽','飞','云','超','忠','亮','延']
                   address:                    '陆家嘴环路1388号16楼' + n.to_s + '座',
                   zipcode:                    '20000' + n.to_s,
                   email:                      'mail.address.' + n.to_s + '@mail.com',
-                  user_id:                    (n+1).to_s,)
+                  user_id:                    ( n + 1 ).to_s,
+                  cellphone:                  '1398888888' + n.to_s)
 end
