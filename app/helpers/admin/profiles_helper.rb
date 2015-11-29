62e47e1b15134e1245f27f48f8e9d48fe1a104c3
Jimmy Xu <jianmin.xu@usingnow.com>
2015-11-29 23:13:51 +0800

    revise the show page of profile in backend system to keep basic info on the top.

--- app/helpers/admin/profiles_helper.rb
+++ app/helpers/admin/profiles_helper.rb
@@ -1,5 +1,13 @@
 module Admin::ProfilesHelper
-  def easyname(f_name, l_name)
+  def easy_name(f_name, l_name)
     return l_name + " " + f_name
+  end
+
+  def easy_gender(gender)
+    return gender ? "男" : "女"
+  end
+
+  def easy_marital_status(mariage)
+    return mariage ? "已婚" : "单身"
   end
 end
