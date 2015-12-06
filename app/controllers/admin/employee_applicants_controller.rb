308f6ca27edac4975d4171fcf7ef7d2de53655b8
SpiderEvgn <spiderz825@hotmail.com>
2015-12-06 23:08:18 +0800

    add controller admin/employee_applicants index to list applicants back-end

--- app/controllers/admin/employee_applicants_controller.rb
+++ app/controllers/admin/employee_applicants_controller.rb
@@ -5,8 +5,15 @@ class Admin::EmployeeApplicantsController < ApplicationController
     @employee_applicants = EmployeeApplicant.all
   end
 
-  private 
-    def employee_applicant_params
-      params.require(:employee_applicant).permit(:employee_snum, :name, :email, :board_on, :acceptance, :created_at)
-    end
+  def destroy
+    @employee_applicants = EmployeeApplicant.find(params[:id])
+    @employee_applicants.destroy
+
+    redirect_to admin_employee_applicants_url
+  end
+
+  # private 
+  #   def employee_applicant_params
+  #     params.require(:employee_applicants).permit(:employee_snum, :name, :email, :board_on, :acceptance, :created_at)
+  #   end
 end
