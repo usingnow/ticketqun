b9b61a0fc8ea84a9a94e7415e7061f5ecc6a33f9
SpiderEvgn <spiderz825@hotmail.com>
2015-12-12 16:07:20 +0800

    add employee_applicants/new presence validation and error notice

--- app/controllers/employee_applicants_controller.rb
+++ app/controllers/employee_applicants_controller.rb
@@ -1,16 +1,22 @@
 class EmployeeApplicantsController < ApplicationController
   
   def new
+    @employee_applicant = EmployeeApplicant.new
   end
 
   def create
     @employee_applicant = EmployeeApplicant.new(employee_applicant_params)
-    @employee_applicant.save
-
-    redirect_to '/employees/sign_in'
+    
+    if @employee_applicant.save
+      redirect_to '/employees/sign_in'
+    else
+      render 'new'
+    end
   end
 
   private
+
+    # Never trust parameters from the scary internet, only allow the white list through.
     def employee_applicant_params
       params.require(:employee_applicant).permit(:employee_snum, :name, :email, 
                                                  :board_on, :acceptance, :created_at)
--- app/models/employee_applicant.rb
+++ app/models/employee_applicant.rb
@@ -1,3 +1,10 @@
 class EmployeeApplicant < ActiveRecord::Base
 
+  validate do |employee_applicant|
+    employee_applicant.errors.add(:base, "员工号不能为空！") if employee_applicant.employee_snum.blank?
+    employee_applicant.errors.add(:base, "邮箱号不能为空！") if employee_applicant.email.blank?
+    employee_applicant.errors.add(:base, "姓名号不能为空！") if employee_applicant.name.blank?
+    employee_applicant.errors.add(:base, "入职日期不能为空！") if employee_applicant.board_on.blank?
+  end
+
 end
